fb3446d06bda09e90f1d86b7c5cb5ba2e3edcf10
Hermelab1 <hermelabirhanu06@gmail.com>
2022-12-09 22:30:54 -0800

    C variable, if else and while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Starting of execution
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
