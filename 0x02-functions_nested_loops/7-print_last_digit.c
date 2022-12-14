c465d8ce6ce7bf1b351cefa30fc18bd71d55b270
Hermelab1 <hermelabirhanu06@gmail.com>
2022-12-14 09:12:10 -0800

    C function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * last_digit - that computes the last digits of the number
+ *
+ * @n: number which we will give the number
+ * Return: lastDigit if success
+ */
+int print_last_digit(int n)
+{
+    int lastDigit = n;
+    
+    lastDigit = lastDigit % 10;
+    
+    if (lastDigit < 0)
+    {
+        lastDigit *= -1;
+    }
+    
+    _putchar(lastDigit + '0');
+    
+    return (lastDigit);
+}
