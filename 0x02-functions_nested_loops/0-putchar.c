e696667dc11bce7660c87c8624096f8e1b19c34c
Hermelab1 <hermelabirhanu06@gmail.com>
2022-12-13 16:14:35 -0800

    c function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int b;
+
+	for (b = 0; b < 9; b++)
+	{
+		_putchar(a[b]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
