567052abe888bd8f5f5f7b9eb5103b9574aa0bd1
Hermelab1 <hermelabirhanu06@gmail.com>
2022-12-14 08:03:02 -0800

    C function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - that computes the absolute value of an integer
+ *
+ * Return: 0 if success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+
+	return (n);
+}
