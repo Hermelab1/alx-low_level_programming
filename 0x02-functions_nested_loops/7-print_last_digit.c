be1aceb599bbedfbcb3889bb281990dacf057ce1
Hermelab1 <hermelabirhanu06@gmail.com>
2022-12-14 17:30:28 -0800

    C function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * last_digit - that computes the last digits of the number
+ * print_last_digit - prints the last digit of a number
  *
- * @n: number which we will give the number
- * Return: lastDigit if success
+ * @n: number whose last digit must be given
+ *
+ * Return: 0 if success
  */
 int print_last_digit(int n)
 {
-    int lastDigit = n;
-    
-    lastDigit = lastDigit % 10;
-    
-    if (lastDigit < 0)
-    {
-        lastDigit *= -1;
-    }
-    
-    _putchar(lastDigit + '0');
-    
-    return (lastDigit);
+	int lastDigit = n;
+
+	lastDigit = lastDigit % 10;
+
+	if (lastDigit < 0)
+	{
+		lastDigit *= -1;
+	}
+
+	_putchar(lastDigit + '0');
+
+	return (lastDigit);
 }
