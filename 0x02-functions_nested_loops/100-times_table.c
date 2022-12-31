ab0a05bf0d823f6f6586935cc2a24311b616428a
Hermelab1 <hermelabirhanu06@gmail.com>
2022-12-31 12:30:59 -0800

    Function nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,23 +2,24 @@
 #include <stdio.h>
 
 /**
- * Print_times_table - used to print n's time table
+ * Print_times_table- is used to print n number of time table
  *
  * @n: numbers for time table
  */
 void print_times_table(int n)
 {
 	int i, j;
-	
+
 	for (i = 0; i <= n; i++)
 	{
-		for(j = 0;j <= n;j++)
+		for (j = 0; j <= n; j++)
 		{
 			printf("%d", i * j);
-			
 			if (j < n)
 			{
 				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(' ');
 			}
 		}
