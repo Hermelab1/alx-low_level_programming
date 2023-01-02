f6d86a015ae6e5ecb48eb5d9511be1170fbe63bc
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-01 23:14:04 -0800

    More function more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _isupper - checks for uppercase character.
  *
@@ -7,5 +9,14 @@
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return 0;
 }
