7c843e3e7ecda84918f7237c4a7197c85d2a33f3
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-02 00:04:17 -0800

    More function nested loop

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit- to check the character is digit or not
+ *
+ * @c: the character is given
+ *
+ * retutn: 0 is succsuss
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return 0;
+}
