12e79b339bc4672dd551364fb83ce04cc175b094
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-14 08:10:23 -0800

    more functions nested loop

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include <stdto.h>
+
+/**
+ * prime - a function is used to calculate prime factors
+ *
+ * @nprime: prime number
+ *
+ * Ruturn: lprime
+ */
+int main(viod)
+{
+	printf("%lu\n", prime(612852475143));
+	
+	return (0);
+}
+int prime(long int nprime)
+{
+	long int n;
+	int iprime, prime = 2;
+
+	n = nprime;
+	while (n != 0)
+	{
+		if (n % prime == 0)
+		{
+			iprime = n;
+			n /= prime;
+			if (n == 1)
+			{
+				break;
+			}
+		}
+		else
+		{
+			prime += 1;
+		}
+	}
+	return (iprime);
+}
+
