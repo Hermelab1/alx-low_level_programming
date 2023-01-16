d70f003cad896250af729b0e39dbea3b06f66ed7
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-16 04:29:49 -0800

    pointer array string

---
+++ 0x05-pointers_arrays_strings/6-putss2.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen - used to get the length of the string
+ *
+ * @s: length
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * puts2 - prints the character of the string
+ *
+ * @str: string value
+ */
+void puts2(char *str)
+{
+	int lint = _strlen(str);
+	int i;
+	
+	for (i = 0; i <= lint; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
