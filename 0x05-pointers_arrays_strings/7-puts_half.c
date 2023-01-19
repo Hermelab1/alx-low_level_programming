3bb79fc15879c87fabb7c42bdc965c8190d41415
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-18 23:35:12 -0800

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: Size of the string
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+
+	return (size);
+}
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: char pointer
+ *
+ * Return: Size of the string
+ */
+void puts_half(char *str)
+{
+	int cLoop, size, limit;
+
+	size = _strlen(str);
+	limit = (size % 2 != 0) ? (size + 1) / 2 : size / 2;
+
+	for (cLoop = limit; cLoop < size; cLoop++)
+	{
+		_putchar(str[cLoop]);
+	}
+	_putchar('\n');
+}
