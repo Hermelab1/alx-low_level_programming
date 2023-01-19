85ab41899c5a18ef8a851332825f324051a52ff3
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-19 06:27:21 -0800

    static libraries

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-
-CFILES=$(find . -name "*.c" -type f)
-
-for file in $CFILES
-do
-	if [[ -f "$file" ]];
-	then
-		gcc -g -O -c $file
-
-		if [[ ! "$?" = "0" ]];
-		then
-			ERROR=true
-		fi
-	fi
-done
-
-rm -Rf *.c
-rm -Rf *.o
-
-OFILES=$(find . -name "*.c" -type f)
-
-ar rc liball.a $OFILES
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
