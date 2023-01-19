c3d1dce876a92806e75062fc2715bb6640419c29
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-18 19:11:34 -0800

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,15 +2,17 @@
 #include <stddef.h>
 
 /**
- * array_iterator - a function given as a parameter on each element of an array
+ * array_iterator - executes a function given as a parameter on each element
+ *                  of an array.
  *
  * @array: printable elements
- * @size: printable elements number @action: printable function
+ * @size: printable elements number
+ * @action: printable function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long cLoop;
-	
+
 	if (!array || !action)
 		return;
 
