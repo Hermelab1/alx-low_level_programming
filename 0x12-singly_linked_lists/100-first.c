5fcdd1bb8a4106d165ea427b36e4baad3ccd05b4
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-26 12:33:48 -0800

    print strings

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_string - to print a strings
+ */
+void print_string(void)
+{
+	char firstline[] = "You're beat! and yet, you must allow,";
+	char secondline[] = "I bore my house upon my back!";
+
+	printf("%s\n%s\n", firstline, secondline);
+}
