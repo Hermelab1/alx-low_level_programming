657417bac5e7a6e4f3c339087838c470f16e34a6
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-26 12:54:54 -0800

    to prints string in assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,17 @@
-section .data
-    msg: db 'Hello, Holberton', 0x0A
+section .rodata
+    fmt: db 'Hello, %s', 10, 0
+    msg:   db 'Holberton', 0
 
 section .text
-    global _start
-
-_start:
-    ; Print the message
-    mov  eax, 4
-    mov  ebx, 1
-    mov  ecx, msg
-    mov  edx, 15
-    int  0x80
-
-    ; Exit cleanly
-    mov  eax, 1
-    mov  ebx, 0
-    int  0x80
+        global main
+        extern printf
+    main:
+        ; printf(fmt, msg)
+        mov rdi, fmt
+        mov rsi, msg
+	; no XMM registers
+	mov rax, 0
+        call printf
+        ; return 0
+        mov rax, 0
+        ret
