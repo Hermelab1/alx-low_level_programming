412f5130624684aa7385553cdecf3c4c5939beee
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-26 05:43:58 -0800

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,18 +13,17 @@ size_t print_list(const list_t *h)
 	{
 		return (0);
 	}
-	if (h->srt == NULL)
+	if (h->str == NULL)
 	{
 		printf("[0] %s\n", "(nil)");
 	}
 	size_t num_nodes = 0;
-	list_t *tmp = h;
 
-	while (tmp != NULL)
+	while (h != NULL)
 	{
-		printf("[%d]%s\n", tmp->len, tmp->str);
+		printf("[%d]%s\n", h->len, h->str);
 		num_nodes++;
-		tmp = tmp->next;
+		h = h->next;
 	}
 	return (num_nodes);
 }
