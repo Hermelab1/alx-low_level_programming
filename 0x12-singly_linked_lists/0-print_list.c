07947a160d81ff654e7139f33099297e6e7124f5
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-26 05:27:17 -0800

    to print elements in the list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - is used to print the element in the list
+ *
+ * @h: list to find
+ * Return: the number of the nodes
+ */
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+	{
+		printf("[0]\n(nil)\n");
+		return (0);
+	}
+
+	size_t num_nodes = 0;
+	list_t *tmp = h;
+
+	while (tmp != NULL)
+	{
+		printf("[%d]%s\n", tmp->len, tmp->str);
+		num_nodes++;
+		tmp = tmp->next;
+	}
+	return (num_nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,9 @@
+C - Singly linked lists
+
+task 0. to print element in the list
+task 1. to returrn the number of element in the list
+Task 2. to add a new node at the begining of the list
+Task 3. to add a new node at the end of the list
+Task 4. to frees a list
+Task 5. to print a string befor the main function is excuted
+Task 6. write in assembly that prints Hello, Holberton
