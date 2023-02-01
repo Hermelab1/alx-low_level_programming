a197276cfed1a4ae19223faada6dd97163fb6c42
Hermelab1 <hermelabirhanu06@gmail.com>
2023-01-31 16:48:17 -0800

    print llist of element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,10 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int node_num = 0;
-
 	if (!h)
 	{
 		return (0);
 	}
 	printf("%d\n", h->n);
-	node_num++;
-	h = h->next;
-	return (node_num);
+	return (print_listint(h->next) + 1);
 }
