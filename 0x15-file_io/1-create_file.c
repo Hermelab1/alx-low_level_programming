ebc104353e48d9782c6935b435cd50964056523c
Hermelab1 <hermelabirhanu06@gmail.com>
2023-02-09 23:40:04 -0800

    to create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * create_file - a function that creates a file
+ *
+ * @filename: name of the file
+ * @text_content: a poiter that have the file content
+ * Return: 1 as a success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	int result;
+	int permission = S_IRUSR | S_IWUSR;
+	
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	file_descriptor = open(filename, O_CREAT | O_TRUNC | O_WRONLY, permission);
+	if (file_descriptor == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		result = write(file_descriptor, text_content, strlen(text_content));
+		if (result == -1)
+		{
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}
