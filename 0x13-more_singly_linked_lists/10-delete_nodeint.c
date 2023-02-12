0667563bf953390dd62ddac5c3213ef0e3d1c4fa
Hermelab1 <hermelabirhanu06@gmail.com>
2023-02-11 23:42:45 -0800

    delete the node at the given index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at the given index
+ *
+ * @head: head of the node
+ * @index: the given index to delete
+ * Return: 1 for succeeded
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *tmp, *node;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+
+	node = *head;
+	for (i = 0; node && i < (index - 1); i++)
+	{
+		node = node->next;
+	}
+	if (node == NULL || node->next == NULL)
+	{
+		return (-1);
+	}
+
+	tmp = node->next;
+	node->next = tmp->next;
+	free(tmp);
+	return (1);
+}
