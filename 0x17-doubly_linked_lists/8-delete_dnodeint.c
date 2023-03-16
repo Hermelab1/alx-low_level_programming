984d84e04e4b46deadfa8f75d45e4c3fb00b6d41
Hermelab1 <hermelabirhanu06@gmail.com>
2023-03-16 12:12:31 -0700

    fix on the 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,36 +11,60 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *before, *current;
-	unsigned int i = 0;
+	Sign up
+		amanz25
+		/
+		alx-low_level_programming
+		Public
+		Code
+		Issues
+		Pull requests
+		Actions
+		Projects
+		Security
+		Insights
+		alx-low_level_programming/0x17-doubly_linked_lists/8-delete_dnodeint.c
+		@amanz25
+		amanz25 betty format
+		 1 contributor
+		 40 lines (36 sloc)  648 Bytes
+#include "lists.h"
 
-	current = *head;
+		 /**
+		  *  * delete_dnodeint_at_index - function that deletes the node at index index
+		  *   * @head: pointer to the list head
+		  *    * @index: index where the node to be deleted
+		  *     *
+		  *      * Return: 1 (Success) or -1 (fail)
+		  *       */
 
-	if (current == NULL)
-		return (-1);
+		 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+		 {
+			 	dlistint_t *t;
 
-	if (index == 0)
-	{
-		*head = current->next;
-		current->prev = NULL;
-		free(current);
-		return (1);
-	}
+					t = *head;
+						if (*head == NULL)
+									return (-1);
+							while (index != 0)
+									{
+												if (t == NULL)
+																return (-1);
+														t = t->next;
+																index--;
+																	}
+								if (t == *head)
+										{
+													*head = t->next;
+															if (*head != NULL)
+																			(*head)->prev = NULL;
+																}
+									else
+											{
+														t->prev->next = t->next;
+																if (t->next != NULL)
+																				t->next->prev = t->prev;
+																	}
 
-	while (i < index)
-	{
-		before = current;
-		if (current->next == NULL)
-			return (-1);
-
-		current = current->next;
-		i++;
-	}
-	before->next = current->next;
-
-	if (current->next != NULL)
-		current->next->prev = before;
-	free(current);
-
-	return (1);
-}
+										free(t);
+											return (1);
+		 }
