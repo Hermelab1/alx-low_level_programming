d8ce20a2656bb53ec024174fbaddbcaee9ad731b
Hermelab1 <hermelabirhanu06@gmail.com>
2023-03-21 00:53:34 -0700

    congra you win the jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -P /tmp https://github.com/Hermelab1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win_jackpot.so
+wget -q -P /tmp https://github.com/Hermelab1/alx-low_level_programming/row/main/0x18-dynamic_libraries/win_jackpot.so
 export LD_PRELOAD=/tmp/win_jackpot.so
